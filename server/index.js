4f0b6f2d085e65a6b7b752c42d42de4d6fef5ccc
Nikhilm-99 <nmanoj@albany.edu>
2024-08-12 22:40:37 +0530

    Render_Deployment

--- server/index.js
+++ server/index.js
@@ -11,6 +11,10 @@ dotenv.config();
 app.use(bodyParser.json({limit : "30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit : "30mb",extended:true}));
 
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "build"))); // put this line of code in app.js
+
 app.use(cors());
 app.use('/posts',postRoutes);
 
