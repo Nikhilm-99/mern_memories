735560bcdf29e397340e53b59d1ed2cd1f530d36
Nikhilm-99 <nmanoj@albany.edu>
2024-08-12 23:02:22 +0530

    Render_Deployment_3

--- server/index.js
+++ server/index.js
@@ -3,27 +3,31 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
 dotenv.config();
-app.use(bodyParser.json({limit : "30mb",extended:true}));
-app.use(bodyParser.urlencoded({limit : "30mb",extended:true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-const path = require("path");
+// Get __dirname in ES6 modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "build"))); // put this line of code in app.js
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, "build")));
 
 app.use(cors());
-app.use('/posts',postRoutes);
+app.use('/posts', postRoutes);
 
-const CONNECTION_URL="mongodb+srv://nikhilmanoj19:zjnbKS5iuRB8sZNh@cluster0.uyncedv.mongodb.net/";
+const CONNECTION_URL = process.env.CONNECTION_URL || "mongodb+srv://nikhilmanoj19:zjnbKS5iuRB8sZNh@cluster0.uyncedv.mongodb.net/";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser: true,useUnifiedTopology:true})
-    .then(()=>app.listen(PORT,()=>console.log(`Server running on Port : ${PORT}`)))
-   .catch((error)=>console.log(error.message)); 
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on Port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
 
-
-//mongoose.set('useFindAndModify',false);
+// mongoose.set('useFindAndModify', false);
